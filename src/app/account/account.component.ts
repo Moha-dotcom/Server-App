fec4c65185853cc909ea3bd08fe03936306c597a
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-06 11:07:12 -0500

    Added new Coount Component

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+
+  // @ts-ignore
+  @Input() account: {name: string, status: string};
+  // @ts-ignore
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    console.log('A server status changed, new status: ' + status);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
